2ae79707b4e59c6b68ef1c2b5f5aa73e8fe72ae3
satyasai69 <ch.satya.sai133@gmail.com>
2024-05-06 14:38:52 +0530

    chain

--- apps/web/src/utils/wagmi.ts
+++ apps/web/src/utils/wagmi.ts
@@ -160,7 +160,7 @@ export const metaMaskConnector = new MetaMaskConnector({
 const bloctoConnector = new BloctoConnector({
   chains,
   options: {
-    defaultChainId: 56,
+    defaultChainId:  11000,
     appId: 'e2f2f0cd-3ceb-4dec-b293-bb555f2ed5af',
   },
 })
